8c907e7d512108d0dad01af039a798ff7ad27320
MishenkovDaniil <111277442+MishenkovDaniil@users.noreply.github.com>
2022-08-23 16:20:22 +0300

    разбитие задачи на несколько функций, assert-ы

--- squareSolver.cpp
+++ squareSolver.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-//#define NDEBUG
 #include <assert.h>
+#include <cmath>
+#define INFSOLVES 4
+//#define NDEBUG
 
-void solve_square (const float a, const float b, const float c);
+void enterSquare (float *a, float *b, float *c);
+int numberOfRoots (const float a, const float b, const float c);
+void solveSquare (const float a, const float b, const float c, const int nRoots, float *x1, float *x2);
 
 int main ()
 {
@@ -11,35 +15,75 @@ int main ()
     float b = 0;
     float c = 0;
 
-    printf ("enter X squared coefficient: ");
-    scanf ("%f", &a);
+    float x1 = 0;
+    float x2 = 0;
 
-    printf ("enter X coefficient: ");
-    scanf ("%f", &b);
+    enterSquare (&a, &b, &c);
 
-    printf ("enter absolute term: ");
-    scanf ("%f", &c);
+    int nRoots = numberOfRoots (a, b, c);
 
-    solve_square (a, b, c);
+    solveSquare (a, b, c, nRoots, &x1, &x2);
 
     return 0;
 }
 
-void solve_square (const float a, const float b, const float c)
+void enterSquare (float *a, float *b, float *c)
 {
-    assert ((b*b - 4*a*c) >= 0);
-    
-    float rootFromD = sqrt(b*b - 4*a*c);
+  printf ("enter X squared coefficient: ");
+  scanf ("%f", a);
+  assert(std::isfinite(*a));
 
-    float x1 = (-b + rootFromD)/2;
-    float x2 = (-b - rootFromD)/2;
-    
-    if (x1 == x2)
+  printf ("enter X coefficient: ");
+  scanf ("%f", b);
+  assert (std::isfinite(*b));
+
+
+  printf ("enter absolute term: ");
+  scanf ("%f", c);
+  assert (std::isfinite(*c));
+}
+
+int numberOfRoots (const float a, const float b, const float c)
+{
+    const float DISCR = b*b - 4*a*c;
+
+    if ((a == 0) && (b == 0) && (c == 0))
     {
-        printf ("the solution is x = %f\n", x1);
+        return INFSOLVES;
+    }
+    else if (DISCR < 0)
+    {
+        return 0;
+    }
+    else if (DISCR == 0)
+    {
+         return 1;
     }
     else
     {
-        printf ("the solutions are x = %f and x = %f\n", x1, x2);
+        return 2;
+    }
+}
+
+void solveSquare (const float a, const float b, const float c, const int nRoots, float *x1, float *x2)
+{
+     const float DISCR = b*b - 4*a*c;
+     const float ROOT_FROM_DISCR = sqrt(DISCR);
+
+     switch (nRoots)
+     {
+        case 0: printf ("there is no solutions");
+                break;
+        case 1: *x1 = *x2 = -b / (2*a);
+                printf ("the solution is x = %f.", *x1);
+                break;
+        case 2: *x1 = (-b + ROOT_FROM_DISCR)/(2*a);
+                *x2 = (-b - ROOT_FROM_DISCR)/(2*a);
+                printf ("the solutions are x = %f and x = %f\n", *x1, *x2);
+                break;
+        case INFSOLVES: printf ("any number");
+                        break;
+        default: printf ("ERROR");
+                 break;
     }
 }
