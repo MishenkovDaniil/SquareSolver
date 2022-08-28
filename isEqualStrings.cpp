154d8e8081469fb47ea017b89bb7c7ed922952ed
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-08-28 17:11:23 +0300

    [5.0] fix bug with entering

--- isEqualStrings.cpp
+++ isEqualStrings.cpp
@@ -3,6 +3,8 @@
  * \brief consists from string comparing function
  */
 
+#include  <ctype.h>
+#include <stdio.h>
 #include <assert.h>
 
 #include "isEqualStrings.h"
@@ -12,13 +14,18 @@ int isEqualStrings (const char str1[], const char str2[])
     assert (str1);
     assert (str2);
 
-    for (int i = 0; str1[i] != '\0'; i++)
+    int i = 0;
+    while (isspace (str1[i]) != 0)
     {
-        if (str1[i] != str2[i])
+        i++;
+    }
+
+    for (int j = 0; str1[i] != ' ' && str1[i] != '\t' && str1[i] != '\0';j++, i++)
+    {
+        if (str1[i] != str2[j])
         {
             return 0;
         }
     }
-
     return 1;
 }
